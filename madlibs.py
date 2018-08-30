d3d41424a9e174e4e83b9d7de12eb720ef8dca05
tkrain23 <tk.rain09@gmail.com>
2018-08-29 17:18:22 -0700

    added program file + began development

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# Name: Timothy C. Kaing
+# Project: Madlibs
+#
+# Requirements:
+# - Display story with blanks for input words with correct type
+# - Ask user to input words to fill in the blanks.
+# - Output the story with the blanks filled in.
+# - The system module (for accessing command-line arguments)
+# - A test suite to test user input.
+
+# initialize variables
+
+story = "There once was a boy named %s. He was a %s child who dreamed of "
++"becoming a %s. One day he decided to %s"
+
+print(story)

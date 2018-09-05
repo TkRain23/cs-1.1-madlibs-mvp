6409eb08c098cf445e31d161229c5e85b68d5e93
tkrain23 <tk.rain09@gmail.com>
2018-09-05 08:02:00 -0700

    added core functionality

--- madlibs.py
+++ madlibs.py
@@ -10,7 +10,33 @@
 
 # initialize variables
 
-story = "There once was a boy named %s. He was a %s child who dreamed of "
-+"becoming a %s. One day he decided to %s"
+story = '''
+The Potatoes and the Tomatoes: Template
+
+There once was a potato named _____. The potato had _____ friends.
+Together they would _____, _____, and _____ the tomatoes. One day the potato
+met a tomato named _____. The potato loved how _____ the tomato was. The potato
+would then _____ the tomato.
+'''
 
 print(story)
+
+noun1 = raw_input("Enter a noun: ")
+adj1 = raw_input("Enter a adjective: ")
+verb1 = raw_input("Enter a verb: ")
+verb2 = raw_input("Enter a verb: ")
+verb3 = raw_input("Enter a verb: ")
+noun2 = raw_input("Enter a noun: ")
+adj2 = raw_input("Enter a adjective: ")
+verb4 = raw_input("Enter a verb: ")
+
+story = '''
+The Potatoes and the Tomatoes: A story by you
+
+There once was a potato named %s. The potato had %s friends.
+Together they would %s, %s, and %s the tomatoes. One day the potato
+met a tomato named %s. The potato loved how %s the tomato was. The potato
+would then %s the tomato.
+'''
+
+print(story) % (noun1, adj1, verb1, verb2, verb3, noun2, adj2, verb4)
